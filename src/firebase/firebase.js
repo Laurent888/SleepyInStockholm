f714ccd0d92b908e33c7d8d0334a411a2a219e27
Laurent Tram <antaresscp@gmail.com>
2020-02-09 14:21:36 +0100

    changed the apikey

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,21 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+let key1, projectID;
+if (process.env.NODE_ENV !== "production") {
+  key1 = process.env.REACT_APP_apiKEY;
+  projectID = process.env.REACT_APP_projectID;
+} else {
+  key1 = process.env.apiKEY;
+  projectID = process.env.projectID;
+}
+
 const config = {
-  apiKey: process.env.REACT_APP_apiKEY,
-  authDomain: process.env.REACT_APP_projectID + ".firebaseapp.com",
-  databaseURL: "https://" + process.env.REACT_APP_projectID + ".firebaseio.com",
-  projectId: process.env.REACT_APP_projectID,
-  storageBucket: process.env.REACT_APP_projectID + ".appspot.com",
+  apiKey: key1,
+  authDomain: projectID + ".firebaseapp.com",
+  databaseURL: "https://" + projectID + ".firebaseio.com",
+  projectId: projectID,
+  storageBucket: projectID + ".appspot.com",
   messagingSenderId: "302960952347",
   appId: "1:302960952347:web:d020adc85760473f0724be"
 };
