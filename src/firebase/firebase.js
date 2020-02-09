560fe7ffd80f0f4d9e1c8303e6a1a55b9e69f996
Laurent Tram <antaresscp@gmail.com>
2020-02-09 14:49:44 +0100

    changed api key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,21 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-let APIKEY, PROJECTID;
-if (process.env.NODE_ENV !== "production") {
-  APIKEY = process.env.REACT_APP_APIKEY;
-  PROJECTID = process.env.REACT_APP_PROJECTID;
-} else {
-  APIKEY = process.env.APIKEY;
-  PROJECTID = process.env.PROJECTID;
-}
+// let APIKEY, PROJECTID;
+// if (process.env.NODE_ENV !== "production") {
+//   APIKEY = process.env.REACT_APP_APIKEY;
+//   PROJECTID = process.env.REACT_APP_PROJECTID;
+// } else {
+//   APIKEY = process.env.APIKEY;
+//   PROJECTID = process.env.PROJECTID;
+// }
 
 const config = {
-  apiKey: APIKEY,
-  authDomain: PROJECTID + ".firebaseapp.com",
-  databaseURL: "https://" + PROJECTID + ".firebaseio.com",
-  projectId: PROJECTID,
-  storageBucket: PROJECTID + ".appspot.com",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_PROJECTID + ".firebaseapp.com",
+  databaseURL: "https://" + process.env.REACT_APP_PROJECTID + ".firebaseio.com",
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_PROJECTID + ".appspot.com",
   messagingSenderId: "302960952347",
   appId: "1:302960952347:web:d020adc85760473f0724be"
 };
